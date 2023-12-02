b8cbb9eab82e8b0d0fd2fba1c3004fb093c9395e
Dylan Moore <dylankmoore@gmail.com>
2023-12-02 09:33:50 -0600

    pain levels functional

--- api/mergedData.js
+++ api/mergedData.js
@@ -4,11 +4,14 @@ import { getSingleLog } from './LogData';
 const viewPainDetails = (logFirebaseKey) => new Promise((resolve, reject) => {
   getSingleLog(logFirebaseKey)
     .then((logObj) => {
-      getPainLevel(logObj.painid)
-        .then((painObject) => {
-          resolve({ painObject, ...logObj });
-        });
+      if (logObj.painid) {
+        getPainLevel(logObj.painid)
+          .then((painObject) => {
+            resolve({ painObject, ...logObj });
+          }).catch((error) => reject(error));
+      } else {
+        resolve({ ...logObj, painObject: null });
+      }
     }).catch((error) => reject(error));
 });
-
 export default viewPainDetails;
--- api/painData.js
+++ api/painData.js
@@ -2,24 +2,38 @@ import { clientCredentials } from '../utils/client';
 
 const endpoint = clientCredentials.databaseURL;
 
-const getPainLevel = () => new Promise((resolve, reject) => {
-  fetch(`${endpoint}/paindata.json`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((response) => response.json())
-    .then((data) => {
-      console.warn('Fetched Data', data);
-      if (data) {
-        resolve(Object.values(data));
-      } else {
-        resolve([]);
-      }
+// GET PAIN LEVELS
+const getPainLevel = (painId = null) => {
+  let url = `${endpoint}/paindata.json`;
+  if (painId) {
+    url = `${endpoint}/paindata/${painId}.json`;
+  }
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
-    .catch(reject);
-});
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          if (painId) {
+            resolve(data);
+          } else {
+            // if the painId isn't provided, resolve w/ array of pain data
+            resolve(Object.values(data));
+          }
+        } else {
+          // resolves an empty array/null if data is N/A
+          resolve(painId ? null : []);
+        }
+      })
+      .catch(reject);
+  });
+};
 
+// UPDATE PAIN LEVELS
 const updatePainLevel = (payload) => new Promise((resolve, reject) => {
   console.warn('endpoint:', endpoint);
   fetch(`${endpoint}/paindata/${payload.firebaseKey}.json`, {
